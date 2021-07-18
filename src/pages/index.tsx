2354f79dac560913977f57221dd347df930e2bf5
Shinsuke Obata <studio15.jp@gmail.com>
2021-07-18 16:13:48 +0900

    Impl submit message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { NextPage, GetStaticProps } from "next";
+import { useRouter } from "next/router";
 
 type IndexProps = {
   texts: Array<{
@@ -10,18 +12,44 @@ type IndexProps = {
   }>;
 };
 
+const baseUrl = "https://versatileapi.herokuapp.com/api";
+
 export const getStaticProps: GetStaticProps<IndexProps> = async () => {
   const res = await fetch(
-    "https://versatileapi.herokuapp.com/api/text/all?$orderby=_created_at%20desc&$limit=20"
+    baseUrl + "/text/all?$orderby=_created_at%20desc&$limit=20"
   );
   const json = await res.json();
   return { props: { texts: json } };
 };
 
+const submitMessage = (message: string) => {
+  fetch(baseUrl + "/text", {
+    method: "POST",
+    headers: { Authorization: "HelloWorld" },
+    body: JSON.stringify({ text: message }),
+  });
+};
+
 const Index: NextPage<IndexProps> = ({ texts }) => {
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+
   return (
     <>
       <h1>WebClient4Engineer</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          submitMessage(message);
+          router.reload();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="What are you coding?"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
       <ul>
         {texts.map((text) => (
           <li key={text.id}>{text.text}</li>
